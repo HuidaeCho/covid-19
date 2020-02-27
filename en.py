0e37764a2db63ec0ee9ca54d4c2332ce9ee51903
Huidae Cho <grass4u@gmail.com>
2020-02-27 10:00:09 -0500

    Update data

--- en.py
+++ en.py
@@ -66,4 +66,23 @@ en = {
 
         '香港': 'Hong Kong',
         '澳门': 'Macau',
+
+        '서울': 'Seoul',
+        '부산': 'Busan',
+        '대구': 'Daegu',
+        '인천': 'Incheon',
+        '광주': 'Gwangju',
+        '대전': 'Daejeon',
+        '울산': 'Ulsan',
+        '세종': 'Sejong',
+        '경기': 'Gyeonggi',
+        '강원': 'Gangwon',
+        '충북': 'Chungbuk',
+        '충남': 'Chungnam',
+        '전북': 'Jeonbuk',
+        '전남': 'Jeonnam',
+        '경북': 'Gyeongbuk',
+        '경남': 'Gyeongnam',
+        '제주': 'Jeju',
+        '검역': 'Quarantine',
 }
