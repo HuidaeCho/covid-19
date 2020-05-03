83b8d6fe49bf4baf85e87eb46e3f0125abc405d6
Huidae Cho <grass4u@gmail.com>
2020-05-03 13:41:33 -0400

    Update Italy fetching

--- fetch_data.py
+++ fetch_data.py
@@ -1260,35 +1260,31 @@ def write_csv():
 
 if __name__ == '__main__':
     if not config.use_local_data_only:
-        #fetch_csse_csv()
-        #fetch_csse_rest()
-        #clean_us_data()
-
-        #try:
-        #    fetch_kcdc_country()
-        #except:
-        #    traceback.print_exc(file=sys.stdout)
-        #try:
-        #    fetch_kcdc_provinces()
-        #except:
-        #    traceback.print_exc(file=sys.stdout)
-        #try:
-        #    fetch_dxy()
-        #except:
-        #    traceback.print_exc(file=sys.stdout)
+        fetch_csse_csv()
+        fetch_csse_rest()
+        clean_us_data()
+        try:
+            fetch_kcdc_country()
+        except:
+            traceback.print_exc(file=sys.stdout)
+        try:
+            fetch_kcdc_provinces()
+        except:
+            traceback.print_exc(file=sys.stdout)
+        try:
+            fetch_dxy()
+        except:
+            traceback.print_exc(file=sys.stdout)
         try:
             fetch_statistichecoronavirus()
         except:
             traceback.print_exc(file=sys.stdout)
-        #try:
-        #    fetch_minsal()
-        #except:
-        #    traceback.print_exc(file=sys.stdout)
-
-    #merge_local_data()
-
-    #sort_data()
-    #report_data()
-
-    #write_geojson()
-    #write_csv()
+        try:
+            fetch_minsal()
+        except:
+            traceback.print_exc(file=sys.stdout)
+    merge_local_data()
+    sort_data()
+    report_data()
+    write_geojson()
+    write_csv()
